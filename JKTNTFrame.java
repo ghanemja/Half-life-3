6e278af0bcc6e50f2507b673345abf2762646bc9
ghanemja <55719558+ghanemja@users.noreply.github.com>
2020-05-03 16:21:57 -0400

    Changed import statements

--- JKTNTFrame.java
+++ JKTNTFrame.java
@@ -1,15 +1,7 @@
 import java.awt.*;
-
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
